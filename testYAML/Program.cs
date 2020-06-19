8fc2db8429a85aff574980d22937e76a657c29a7
COPathetic <67140417+COPathetic@users.noreply.github.com>
2020-06-19 09:29:34 +0200

    Add files via upload

---
+++ testYAML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testYAML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App compiled..!");
+        }
+    }
+}
